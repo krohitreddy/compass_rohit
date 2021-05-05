f56976cd630d5846d65803b57eba92128bb31da0
rohit kommareddy <rohit.kommareddy@compass.com>
2021-05-05 18:44:43 -0400

    test

--- src/main/java/com/urbancompass/rohitTest/App.java
+++ src/main/java/com/urbancompass/rohitTest/App.java
@@ -1,14 +1,29 @@
 package com.urbancompass.rohitTest;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class App {
 
 	public static void main(String[] args) {
-		
+
 		try {
-			S3Extractor.process(29, -1, -1);
+			// S3Extractor.process(29, -1, -1);
+			justCount();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
+	}
+
+	private static void justCount() {
+		try {
+			BufferedReader reader = new BufferedReader(
+					new FileReader("/home/rohit.kommareddy/development/rohitTest/finalFile.tsv"));
+			System.out.println(reader.lines().count());
+			reader.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
